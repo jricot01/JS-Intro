754b0217865300fb10223beeffcce1f197e57b3f
jricot01 <jricot01@gmail.com>
2022-08-13 00:07:25 -0400

    Completed JS Intro

---
+++ app.js
@@ -0,0 +1,56 @@
+// Exercise 1&2
+const firstName = "Jayden";
+let lastName = "Ricot";
+var age = 19;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+lastName = "Ricot";
+age = "19";
+console.log(lastName);
+console.log(age);
+
+//---------------------------------------------//
+
+//Exercise 3
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(language);
+console.log(createdYear);
+console.log(isCaseSensitive);
+
+//---------------------------------------------//
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(price);
+console.log(isOnSale);
+console.log(salePercentage);
+console.log(stock);
+console.log(inStock);
+console.log(selectedSize);
+
+//---------------------------------------------//
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(title);
+console.log(author);
+console.log(pageCount);
+console.log(bookmark);
+console.log(hasRead);
